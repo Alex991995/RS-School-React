8505b365e742fa47368122aa21aa49331d5480e6
Alex991995 <sashe.1995@gmail.com>
2024-07-06 18:39:00 +0300

    feat: i change repo

---
+++ class-components/src/components/Loader.tsx
@@ -0,0 +1,10 @@
+import { Component } from 'react';
+import styles from '../styles/Loader.module.css';
+
+class Loader extends Component {
+  render() {
+    return <div className={styles.loader}></div>;
+  }
+}
+
+export default Loader;
---
+++ class-components/src/types/fetchTypes.ts
@@ -0,0 +1,20 @@
+export type ArrayProducts = Product[];
+
+export interface Product {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  images: string[];
+  creationAt: string;
+  updatedAt: string;
+  category: Category;
+}
+
+export interface Category {
+  id: number;
+  name: string;
+  image: string;
+  creationAt: string;
+  updatedAt: string;
+}
