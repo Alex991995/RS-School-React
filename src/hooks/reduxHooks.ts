264c30de88e23aa5953f59a64fe67bc8dc4e43c7
Aleksandr Kisel <51911663+Alex991995@users.noreply.github.com>
2024-08-12 15:38:06 +0300

    feat: starte doing forms app

--- src/hooks/reduxHooks.ts
+++
@@ -1,5 +0,0 @@
-import { useDispatch, useSelector } from 'react-redux';
-import type { RootState, AppDispatch } from '../features/store';
-
-export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
-export const useAppSelector = useSelector.withTypes<RootState>();
--- src/utils/constants.ts
+++
@@ -1,3 +0,0 @@
-export const allPages = 47;
-
-export const baseUrl = 'https://api.escuelajs.co/api/v1/products';
